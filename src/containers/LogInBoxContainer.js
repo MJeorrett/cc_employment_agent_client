f3cc771645f0708983328b644e05ad4fba9ce1ef
Matthew Jeorrett <matthewjeorrett@gmail.com>
2017-01-31 11:18:17 +0000

    removing debuggin elements

--- src/containers/LogInBoxContainer.js
+++ src/containers/LogInBoxContainer.js
@@ -10,10 +10,7 @@ class LogInBoxContainer extends React.Component {
     return (
       <div>
         <LogInBox onLogIn={ this.props.logInUser }/>
-        <p>logged_in_user: { JSON.stringify( this.props.logged_in_user ) }</p>
-        <p>log_in_in_progress: { this.props.log_in_in_progress.toString() }</p>
-        <p>log_in_error: { this.props.log_in_error.toString() }</p>
-        <p>testing</p>
+        <p>{ this.props.log_in_error.toString() }</p>
       </div>
     )
   }
